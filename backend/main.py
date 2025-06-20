15f225f371cca7acde2abdb8b7af45eaf39ad957
simne7 <simne7@gmail.com>
2025-06-20 16:33:06 +0200

    ci: read allowed origins from environment variable

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(" ")
 
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
